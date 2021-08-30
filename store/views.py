0b3a52a3a37fbff7e90b9c378bfec733868ff6bb
IgnazioSotgiu <ignazio_sotgiu@yahoo.it>
2021-08-30 20:11:12 +0000

    Created homepage view - index.html

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def display_homepage(request):
+    template = 'store/index.html'
+    return render(request, template)
