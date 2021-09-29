2b1234588de99ff2563885312ebcf1687f4489c2
IgnazioSotgiu <ignazio_sotgiu@yahoo.it>
2021-09-29 22:33:36 +0000

    Correct url for homepage set debug to false in production env

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('/', views.display_homepage, name='home'),
+     path('', views.display_homepage, name='home'),
      path('store/', views.products, name='products'),
      path('<int:product_id>/', views.single_product, name='single_product'),
      path('search/', views.search_result, name='search_result'),
