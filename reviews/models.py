a2ffd46fab66207dedaf2b60f2db08d6a7582da3
IgnazioSotgiu <ignazio_sotgiu@yahoo.it>
2021-10-21 01:58:54 +0000

    changed review model textfield for review_text

--- reviews/models.py
+++ reviews/models.py
@@ -7,7 +7,7 @@ class Review(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='rewiews')
     review_title = models.CharField(max_length=254, null=False, blank=False)
-    review_text = models.CharField(max_length=1024, null=False, blank=False)
+    review_text = models.TextField(max_length=1024, null=False, blank=False)
     review_date = models.DateField(auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.PROTECT, related_name='user_reviews')
